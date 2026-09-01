4aff5e90c47c8737b39ed2a5a2db7ec029f353cb
Mateo Lugo Echeverri <2083785@cegepmontpetit.ca>
2026-09-01 10:22:09 -0400

    FCT Utilisation du LastFm Api

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@
 
 import { useState } from "react";
 import { Song } from "./_types/song";
+import axios from "axios";
 
 export default function Home() {
 
@@ -12,15 +13,34 @@ export default function Home() {
   // Pour afficher les données
   const [similarArtists, setSimilarArtists] = useState<string[]>([]);
   const [songs, setSongs] = useState<Song[]>([]);
+  
+
+  const [api , setApi] = useState("9a8a3facebbccaf363bb9fd68fa37abf")
 
   // Requête #1
   async function getSimilarArtists(){
-
+    const response = await axios.get("http://ws.audioscrobbler.com/2.0/?method=artist.getsimilar&artist="+ artistName +"&limit=20&api_key="+ api +"&format=json")
+    console.log(response.data)
+    
+    let artistList : string[] = [];
+    
+    for(let a of response.data.similarartists.artist){
+      artistList.push(a.name)
+    }
+    
+    setSimilarArtists(artistList)
   }
 
   // Requête #2
   async function getTopSongs(){
+    const response = await axios.get("http://ws.audioscrobbler.com/2.0/?method=tag.gettoptracks&tag="+ genre +"&limit=20&api_key="+ api +"&format=json")
+    console.log(response.data)
 
+    let songList : Song[] = [];
+    for(let s of response.data.tracks.track){
+      songList.push(new Song(s.name, s.artist.name, s.duration))
+    }
+    setSongs(songList)
   }
 
   return (
@@ -36,17 +56,18 @@ export default function Home() {
           {/* Formulaire */}
           <div className="flex items-center">
             <span className="font-bold">Artiste :</span>
-            <input type="text" className="px-1 py-0.5 bg-zinc-100 rounded-sm border-1 mx-2 text-zinc-900 text-sm border-zinc-500" placeholder="Nana Mouskouri" />
-            <button className="bg-zinc-300 rounded-md border-zinc-500 border-1 cursor-pointer px-2 py-0 text-zinc-900 active:bg-zinc-400">Chercher</button>
+            <input onChange={(e) => setArtistName(e.target.value)} type="text" className="px-1 py-0.5 bg-zinc-100 rounded-sm border-1 mx-2 text-zinc-900 text-sm border-zinc-500" placeholder="Nana Mouskouri" />
+            <button onClick={getSimilarArtists} className="bg-zinc-300 rounded-md border-zinc-500 border-1 cursor-pointer px-2 py-0 text-zinc-900 active:bg-zinc-400">Chercher</button>
           </div>
           <hr className="text-zinc-400 my-2" />
 
           {/* Données */}
           <div className="text-xl">Résultats :</div>
           <ul className="list-disc ml-4 text-sm">
-            <li>NOM_ARTISTE</li>
-            <li>NOM_ARTISTE</li>
-            <li>etc.</li>
+            {similarArtists.map(
+              (i) => <li key={i}>{i}</li>
+            )}
+            
           </ul>
         </div>
 
@@ -56,8 +77,8 @@ export default function Home() {
           {/* Formulaire */}
           <div className="flex items-center">
             <span className="font-bold">Genre :</span>
-            <input type="text" className="px-1 py-0.5 bg-zinc-100 rounded-sm border-1 mx-2 text-zinc-900 text-sm border-zinc-500" placeholder="pop" />
-            <button className="bg-zinc-300 rounded-md border-zinc-500 border-1 cursor-pointer px-2 py-0 text-zinc-900 active:bg-zinc-400">Chercher</button>
+            <input onChange={(e) => setGenre(e.target.value)} type="text" className="px-1 py-0.5 bg-zinc-100 rounded-sm border-1 mx-2 text-zinc-900 text-sm border-zinc-500" placeholder="pop" />
+            <button onClick={getTopSongs} className="bg-zinc-300 rounded-md border-zinc-500 border-1 cursor-pointer px-2 py-0 text-zinc-900 active:bg-zinc-400">Chercher</button>
           </div>
 
           <hr className="text-zinc-400 my-2" />
@@ -65,9 +86,9 @@ export default function Home() {
           {/* Données */}
           <div className="text-xl">Résultats :</div>
           <ul className="list-disc ml-4 text-sm">
-            <li>NOM_CHANSON de NOM_ARTISTE (DURÉE_CHANSON secondes)</li>
-            <li>NOM_CHANSON de NOM_ARTISTE (DURÉE_CHANSON secondes)</li>
-            <li>etc.</li>
+            {songs.map(
+              (i) => <li key={i.name}>{i.name} | {i.artist} | ({i.duration} secondes)</li>
+            )}
           </ul>
         </div>
 
